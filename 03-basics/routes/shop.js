f758f5ef5ab347b84cb101ee69f8cc9096a5e2b5
Jin Woo Han <jin00woo@naver.com>
2023-04-18 21:28:05 +0900

    Started using template engines for html.

--- 03-basics/routes/shop.js
+++ 03-basics/routes/shop.js
@@ -2,9 +2,10 @@ const express = require("express");
 const path = require("path");
 const rootDir = require("../util/path");
 const router = express.Router();
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop");
 });
 
 module.exports = router;
